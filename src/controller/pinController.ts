f18023c6087920d1ccd5df38b4696fa49007fc97
kkyu0718 <net605@naver.com>
2022-11-18 01:27:16 +0900

    feat: pin 전체 조회 기능 추가

--- src/controller/pinController.ts
+++ src/controller/pinController.ts
@@ -1,5 +1,17 @@
-const pinController = {
+import { Request, Response } from "express";
+import { pinService } from "../service";
 
+//* 모든 핀 조회
+const getManyPins = async (req: Request, res: Response) => {
+    const pins = await pinService.getManyPins();
+    const data = {
+        pins: pins
+    }
+    return res.status(200).json({ status: 200, message: "핀 전체 조회 성공", data});
+}
+
+const pinController = {
+    getManyPins,
 };
 
 export default pinController;
--- src/service/pinService.ts
+++ src/service/pinService.ts
@@ -1,7 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
+const getManyPins = async () => {
+    const data = await prisma.pin.findMany();
+    return data;
+}
+
 const pinService = {
+    getManyPins,
 };
 
 export default pinService;
