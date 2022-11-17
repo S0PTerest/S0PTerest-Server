92a846f6c10dd71d9ca1f52989fe7cff9f3c7195
kkyu0718 <net605@naver.com>
2022-11-18 01:23:41 +0900

    feat: user 조회 기능 추가

--- src/service/userService.ts
+++ src/service/userService.ts
@@ -2,12 +2,40 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 //* userId로 유저 조회
-const getUserById = () => {
+const getUserById = async (userId: string) => {
+    const user = await prisma.user.findUnique({
+    where: {
+        uid: userId,
+    },
+    });
+
+    return user;
+};
+
+const countFollowerById = async (userId: string) => {
+    const follower = await prisma.userRelation.count({
+        where: {
+            followerId : userId,
+        },
+    });
     
+    return follower;
+}
+
+const countFollowingById = async (userId: string) => {
+    const following = await prisma.userRelation.count({
+        where: {
+            followingId: userId,
+        }
+    });
+
+    return following;
 }
 
 const userService = {
-  getUserById,
+    getUserById,
+    countFollowerById,
+    countFollowingById
 };
 
 export default userService;
