0faf708aed1dc5dca415ebe643fefce8b2b7e6c4
HoYoungChun <ho723@naver.com>
2022-11-23 23:37:03 +0900

    feat: add 4XX error message in updateBoardNote

--- src/controller/boardController.ts
+++ src/controller/boardController.ts
@@ -116,6 +116,36 @@ const createBoardNote = async (req: Request, res: Response) => {
 const updateBoardNote = async (req: Request, res: Response) => {
   const { boardId, noteId } = req.params;
   const { title, description, date, pinIds } = req.body;
+
+  // boardId가 존재하는 uid인지 체크하고 404 반환해주기
+  const board = await boardService.getBoardById(boardId);
+  if (!board) {
+    return res.status(404).json({
+      status: 404,
+      message: boardId + "는 존재하지 않는 Board의 Id입니다",
+    });
+  }
+
+  // pinIds에서 존재하는 uid인지 체크하고 404 반환해주기
+  const validPinCount = await boardService.getValidPinCount(boardId, pinIds);
+  if (validPinCount != pinIds.length) {
+    return res.status(404).json({
+      status: 404,
+      message:
+        "Id가 " +
+        boardId +
+        "인 Board에 속하지 않거나 존재하지 않는 Pin의 Id가 있습니다",
+    });
+  }
+
+  // date 형식 체크하기
+  if (date.match(/^\d{4}-\d{2}-\d{2}$/) == null) {
+    return res.status(400).json({
+      status: 400,
+      message: "date 형식이 YYYY-MM-DD가 아닙니다",
+    });
+  }
+
   const note = await boardService.updateBoardNote(
     noteId,
     boardId,
